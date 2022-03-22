1e3aed7b087f88b0653ccf641f7df1da2ada9a47
Vansh <vanshg2001@protonmail.com>
2022-03-22 06:49:36 -0700

    First draft, formatted

--- libsponge/wrapping_integers.cc
+++ libsponge/wrapping_integers.cc
@@ -14,9 +14,9 @@ using namespace std;
 //! \param n The input absolute 64-bit sequence number
 //! \param isn The initial sequence number
 WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
-    //DUMMY_CODE(n, isn);
-    //return WrappingInt32{0};
-    return WrappingInt32((isn.raw_value() + uint32_t(n))%(1l<<32));
+    // DUMMY_CODE(n, isn);
+    // return WrappingInt32{0};
+    return WrappingInt32((isn.raw_value() + uint32_t(n)) % (1l << 32));
 }
 
 //! Transform a WrappingInt32 into an "absolute" 64-bit sequence number (zero-indexed)
@@ -30,11 +30,12 @@ WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
 //! and the other stream runs from the remote TCPSender to the local TCPReceiver and
 //! has a different ISN.
 uint64_t unwrap(WrappingInt32 n, WrappingInt32 isn, uint64_t checkpoint) {
-    //DUMMY_CODE(n, isn, checkpoint);
-    //if (n-isn<0) uint64_t tmp = uint64_t(n-isn+(1ull<<32)); //INT_MAX but I am not sure if we are allowed to import math
-    //else uint64_t tmp = uint64_t(n-isn);
-    uint64_t tmp = n.raw_value()-wrap(checkpoint, isn).raw_value();
-    uint64_t absSn = tmp+checkpoint;
-    if ((tmp>= (1l<<31)) && (absSn>=(1l<<32))) absSn-=1l<<32;
+    // DUMMY_CODE(n, isn, checkpoint);
+    // if (n-isn<0) uint64_t tmp = uint64_t(n-isn+(1ull<<32)); //INT_MAX but I am not sure if we are allowed to import
+    // math else uint64_t tmp = uint64_t(n-isn);
+    uint64_t tmp = n.raw_value() - wrap(checkpoint, isn).raw_value();
+    uint64_t absSn = tmp + checkpoint;
+    if ((tmp >= (1l << 31)) && (absSn >= (1l << 32)))
+        absSn -= 1l << 32;
     return absSn;
 }
